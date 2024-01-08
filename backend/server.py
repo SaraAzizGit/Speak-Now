3e2082f540c747a08a57fa6f4cba412222ef3223
saraazizhere <saraaziz180903@gmail.com>
2024-01-08 17:55:58 +0500

    upload implemented

--- backend/server.py
+++ backend/server.py
@@ -34,7 +34,7 @@ def get_feedback():
     nervousness = round(nervousness, 2)
     confidence = round(confidence, 2)
 
-    return jsonify({"nervousness": 0.7, "confidence": 0.3})
+    return jsonify({"nervousness": nervousness, "confidence": confidence})
 
 
 
